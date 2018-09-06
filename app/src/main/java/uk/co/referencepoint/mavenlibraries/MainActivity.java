1fe0f3aafd92a26ea34b9aad01fa4d57cdea4e95
nick.wright <nick.wright@referencepoint.co.uk>
2018-09-06 11:32:46 +0100

    removed appcompat reference

--- app/src/main/java/uk/co/referencepoint/mavenlibraries/MainActivity.java
+++ app/src/main/java/uk/co/referencepoint/mavenlibraries/MainActivity.java
@@ -2,6 +2,8 @@ package uk.co.referencepoint.mavenlibraries;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import uk.co.referencepoint.publishtest.*;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Helpers.getCurrentDateStringPlusMinutes(1, "xxx");
     }
 }
